27935226844418c53455bb3159f890192c72765d
Joshua <50963606@qq.com>
2020-08-27 15:37:45 +0800

    2020年8月27日15:08:13

--- reflect/src/com/woniuxy/reflect/demo1/FieldDemo1.java
+++ reflect/src/com/woniuxy/reflect/demo1/FieldDemo1.java
@@ -6,18 +6,16 @@ import java.lang.reflect.Field;
 import java.lang.reflect.Modifier;
 
 public class FieldDemo1 {
-
-    public static void main(String[] args) throws ClassNotFoundException {
-        //
+@SuppressWarnings("all")
+    public static void main(String[] args) {
         Class c1 = User.class;
-        Class c2 = Class.forName("com.woniuxy.reflect.model.User"); // .forName方法会导致类加载，会执行类中的静态代码块
-        Class c3 = new User().getClass();
+//        Class c2 = Class.forName("com.woniuxy.reflect.model.User"); // .forName方法会导致类加载，会执行类中的静态代码块
+//        Class c3 = new User().getClass();
 
 //        System.out.println(c1);
 //        System.out.println(c2);
 //        System.out.println(c3);
 
-        Field[] fields = c1.getFields();    //public
         Field[] fields1 = c1.getDeclaredFields();   //所有的
         for (Field field : fields1) {
             System.out.println(field.getName());    //  获取属性名
@@ -29,9 +27,9 @@ public class FieldDemo1 {
 
         //反编译类的属性
         StringBuilder sb = new StringBuilder();
-        sb.append(Modifier.toString(c1.getModifiers()) + "class" + c1.getSimpleName() + "{\n");
+        sb.append(Modifier.toString(c1.getModifiers())).append("class").append(c1.getSimpleName()).append("{\n");
         for (Field field : c1.getDeclaredFields()) {
-            sb.append("\t" + Modifier.toString(field.getModifiers())+ " " + field.getType().getSimpleName()+ " "  + field.getName() + ";\n");
+            sb.append("\t").append(Modifier.toString(field.getModifiers())).append(" ").append(field.getType().getSimpleName()).append(" ").append(field.getName()).append(";\n");
         }
 
         sb.append("}");
